4102dcc5c01ffc0bd17d2de6ab13d412422d6bed
gluten <goulvenschaal@me.com>
2014-08-18 10:51:12 +0200

    Mise à jour du cours à 10h50

--- Date/date.md
+++ Date/date.md
@@ -4,7 +4,19 @@ Passage des anciennes fontions procédurales à des objets depuis PHP5 qui évit
 
 ## Fonctions de date procédurales
 
-- `time()` : affiche le *timestamp*
+- `time()` : affiche le *timestamp*.
 - `date('Y-m-d')` : affiche la date au format ISO en fonction de la timezone (`tz`) sélectionnée sur le serveur au niveau de `php.ini` ET du serveur via la commande `tzselect`.
+- `gmdate('Y-m-d')` : équivalente à la précédente mais utilise le méridien de Greenwich.
 
+Les deux fonctions précédentes peuvent être utilisées avec un paramètre de *timestamp* UNIX, ex : `date('Y-m-d H:i:s', 125545)`.
 
+Il est possible de générer le *timestamp* à partir d’une date avec la fonction `mktime()`.
+De manière similaire, il existe une fonction `gmmktime` pour générer à partir de GMT.
+
+### Format de dates
+
+- `Y-m-D H:i:s` donnera "2014-08-Mon 12:30:00".
+- `Y-m-j H:i:s` donnera "2014-08-18 12:30:00".
+- `l jS m Y \a\t H:i:s` donnera "Monday 18th 08 2014 at 12:30:00".
+- 'N' donnera "1". Représentation numérique ISO-8601 du jour de la semaine (ajouté en PHP 5.1.0).
+- 'z' donnera "229". Jour de l’année.
--- Date/src/public/index.php
+++ Date/src/public/index.php
@@ -7,11 +7,34 @@ ini_set('date.timezone', 'Europe/Paris');
 
 echo '<pre>';
 
+
 // Afficher timestamp avec PHP
 echo time() . PHP_EOL;
 
-// Afficher date avec PHP
-echo date('Y-m-d') . PHP_EOL;
+// Afficher date avec PHP en se basant sur la timezone actuelle
+echo date('Y-m-d H:i:s') . PHP_EOL;
+
+// Afficher date GMT (Greenwich Mean Time) avec PHP
+echo date('Y-m-d H:i:s') . PHP_EOL;
+
+// Peut être utilisé avec un paramètre de timestamp UNIX
+echo date('Y-m-d H:i:s', 125545) . PHP_EOL; // ATTENTION !
+echo gmdate('Y-m-d H:i:s', 125545) . PHP_EOL;
+
+// Générer le timestamp à partir d’une date
+echo mktime(10,30,0,8,18,2014) . PHP_EOL;
+echo date('Y-m-d H:i:s', mktime(10,30,0,8,18,2014)) . PHP_EOL;
+echo gmdate('Y-m-d H:i:s', mktime(10,30,0,8,18,2014)) . PHP_EOL;
+echo date('Y-m-d H:i:s', gmmktime(10,30,0,8,18,2014)) . PHP_EOL;
+
+
+echo '<h2>Formats</h2>';
+
+echo date('Y-m-D H:i:s', gmmktime(10,30,0,8,18,2014)) . PHP_EOL;
+echo date('Y-m-j H:i:s', gmmktime(10,30,0,8,18,2014)) . PHP_EOL;
+echo date('l jS m Y \a\t H:i:s', gmmktime(10,30,0,8,18,2014)) . PHP_EOL; // Certains caractères doivent être échappés
+echo date('N') . PHP_EOL;
+echo date('z') . PHP_EOL; // jour de l’année
 
 
 echo '</pre>';
